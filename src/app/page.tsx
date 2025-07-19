da69faf599b57e8bbec34fb74d5c678b9a89c835
neel-kumar-v <greenworldwins@gmail.com>
2025-07-19 14:39:43 -0400

    Update Tailwind imports and add custom scrollbar styles
    Replaces Tailwind's @tailwind directives with @import statements and adds custom scrollbar styles in globals.css. Removes redundant overflow-scroll from a div in page.tsx to rely on default or custom scrollbar styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Page() {
             </div>
           </div>
 
-          <div className="h-full overflow-scroll overflow-x-hidden">
+          <div className="h-full overflow-x-hidden">
             <div className="mx-auto max-w-full px-2 py-8 transition-[padding] duration-500 ease-in-out sm:px-4 lg:px-8">
               <div className="grid 3xl:grid-cols-5 grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:grid-cols-4">
                 <JobBoard />
