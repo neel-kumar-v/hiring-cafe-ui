5140646843155dfa028ffe6bd41b3d10af731308
neel-kumar-v <greenworldwins@gmail.com>
2025-07-20 15:13:39 -0400

    Improve UI transitions and styling for Accordion, Dialog, and Drawer
    Enhanced the Accordion component with smoother transitions, improved hover and focus states, and reordered exports. Updated Dialog and Drawer components to explicitly remove unwanted focus ring styling by adding '!ring-0' to their content containers.

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -56,7 +56,7 @@ function DrawerContent({
 			<DrawerPrimitive.Content
 				className={cn(
 					"group/drawer-content fixed z-50 flex h-[90vh] transition-all duration-300 flex-col bg-background",
-					"data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:max-h-[90vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t dark:border-t-neutral-800 border-t-neutral-200",
+					"data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:max-h-[90vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t dark:border-t-neutral-800 border-t-neutral-200 !ring-0",
 					className
 				)}
 				data-slot="drawer-content"
