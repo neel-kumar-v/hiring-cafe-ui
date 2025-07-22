e9234746d9a1f08a7b42ba3f350ed480e5c21c40
neel-kumar-v <greenworldwins@gmail.com>
2025-07-22 00:08:02 -0400

    Add hover scale effect to LabelCheckbox
    Enhanced the Checkbox component in LabelCheckbox to scale up on hover using Tailwind CSS classes. This improves visual feedback for users interacting with the checkbox.

--- src/components/search/filters/util/LabelCheckbox.tsx
+++ src/components/search/filters/util/LabelCheckbox.tsx
@@ -2,9 +2,9 @@ import { Checkbox } from "@/components/ui/checkbox";
 
 export default function LabelCheckbox({ label, checked, onChange }: { label: string, checked: boolean, onChange: (checked: boolean | "indeterminate") => void }) {
   return (
-    <label className="flex items-center gap-2">
-      <Checkbox className="accent-pink-600 size-4" checked={checked} onCheckedChange={onChange} />
-      <span className="text-base">{label}</span>
+    <label className="flex items-center gap-2 group">
+      <Checkbox className="accent-pink-600 size-4 group-hover:scale-125 transition-all duration-300 ease-out" checked={checked} onCheckedChange={onChange} />
+      <span className="text-base ">{label}</span>
     </label>
   );
 }
